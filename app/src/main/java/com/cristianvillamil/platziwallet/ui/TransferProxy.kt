347a8d7fc49a4313e1c8a418206c6954dd274ed1
Liliana Gomez <gomez.liliana333@gmail.com>
2022-06-24 16:26:12 -0400

    Protection proxy design pattern

---
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/TransferProxy.kt
@@ -0,0 +1,26 @@
+package com.cristianvillamil.platziwallet.ui
+
+class TransferProxy {
+    val MAX_TRANSFER_AMOUNT = 1000000
+    val MIN_TRANSFER_AMOUNT = 100
+
+    fun checkTransfer(amount: Double) : String {
+        return when {
+            amount > MAX_TRANSFER_AMOUNT -> {
+                "The amount exceeds maximum allowed"
+            }
+            amount < MIN_TRANSFER_AMOUNT -> {
+                "Minimum transaction is $MIN_TRANSFER_AMOUNT"
+            }
+            else -> {
+                doTransfer(amount)
+                "Transfer completed"
+            }
+        }
+    }
+
+    private fun doTransfer(amount: Double) {
+        
+    }
+
+}
