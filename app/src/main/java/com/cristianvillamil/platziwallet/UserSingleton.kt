0e11f61c24421fadd64441342f8c0b23c1703183
Liliana Gomez <gomez.liliana333@gmail.com>
2022-06-24 11:26:17 -0400

    first commit - Singleton

---
+++ app/src/main/java/com/cristianvillamil/platziwallet/UserSingleton.kt
@@ -0,0 +1,16 @@
+package com.cristianvillamil.platziwallet
+
+class UserSingleton {
+    var username = "Liliana"
+
+    companion object {
+        private var instance : UserSingleton? = null
+
+        fun getInstance() : UserSingleton {
+            if (instance == null) {
+                instance = UserSingleton()
+            }
+            return instance as UserSingleton
+        }
+    }
+}
---
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/home/presenter/HomePresenter.kt
@@ -0,0 +1,21 @@
+package com.cristianvillamil.platziwallet.ui.home.presenter
+
+import com.cristianvillamil.platziwallet.UserSingleton
+import com.cristianvillamil.platziwallet.ui.home.FavoriteTransfer
+import com.cristianvillamil.platziwallet.ui.home.HomeContract
+import com.cristianvillamil.platziwallet.ui.home.data.HomeInteractor
+
+class HomePresenter(private val view : HomeContract.View ) : HomeContract.Presenter {
+    private val homeInteractor = HomeInteractor()
+
+    override fun retrieveFavoriteTransfers() {
+        view.showLoader()
+        homeInteractor.retrieveFavoriteTransfersFromCache(object : HomeContract.OnResponseCallback {
+            override fun onResponse(favoriteList: List<FavoriteTransfer>) {
+                UserSingleton.getInstance().username = "Hello"
+                view.hideLoader()
+                view.showFavoriteTransfers(favoriteList)
+            }
+        } )
+    }
+}
